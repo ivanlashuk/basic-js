fd67599f00e41c9f0b8a454dff43b85b2dcee5ee
Ivan Lashuk <ivan.lashuk2013@gmail.com>
2021-03-09 23:36:29 +0300

    feat: add decision of the task what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,23 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function getSeason(date) {
+    if (date == null) {
+        return 'Unable to determine the time of year!'
+    }
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+    if (Object.prototype.toString.call(date) != '[object Date]') {
+        throw new Error('THROWN');
+    }
+
+    const number = date.getMonth();
+    console.log(number);
+
+    if (number === 11 || number === 0 || number === 1) {
+        return ('winter');
+    } else if (number === 2 || number === 3 || number === 4) {
+        return ('spring');
+    } else if (number === 5 || number === 6 || number === 7) {
+        return ('summer');
+    } else if (number === 8 || number === 9 || number === 10) {
+        return ('autumn');
+    }
+
+}
