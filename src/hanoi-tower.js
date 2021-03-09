6ac431e4174319e4fa9b893f82949ccf74356826
Ivan Lashuk <ivan.lashuk2013@gmail.com>
2021-03-09 23:47:31 +0300

    feat: add decision of the task hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    const turn = (2 ** disksNumber) - 1;
+    const sec = (turnsSpeed / 60) / 60;
+    const second = Math.floor(turn / sec);
+    let value = {};
+    value['turns'] = turn;
+    value['seconds'] = second;
+    return value;
 };
